7b9397f03bff7bf614602d6a40abcfdab9719701
Lukasz <lukaszprzybylka@gmail.com>
2023-09-02 16:13:35 +0100

    feat & style: added clickable tabs showing different content using useState & added styling for active / inactive tabs

--- src/components/MatchDetails/index.js
+++ src/components/MatchDetails/index.js
@@ -1,35 +1,40 @@
+import { useState } from "react"
 import styles from "./MatchDetails.module.css"
 
 export const MatchDetails = () => {
-  return (
-    <>
-    <ul className={styles.nav}>
-        <li className={styles.item}>Overview</li>
-        <li className={styles.item}>General</li>
-        <li className={styles.item}>Team A</li>
-        <li className={styles.item}>Team B</li>
-        <li className={styles.item}>Player</li>
-    </ul>
-    <div>
-        <h4>Overview</h4>
-        <p>Work in progress</p>
-    </div>
-    <div>
-        <h4>General</h4>
-        <p>Work in progress</p>
-    </div>
-    <div>
-        <h4>Team A</h4>
-        <p>Work in progress</p>
-    </div>
-    <div>
-        <h4>Team B</h4>
-        <p>Work in progress</p>
-    </div>
-    <div>
-        <h4>Player</h4>
-        <p>Work in progress</p>
-    </div>
-    </>
-  )
+    const [activeTab, setActiveTab] = useState("General")
+    const changeActiveTab = (tabName) => {
+        return setActiveTab(tabName)
+    }
+    return (
+        <>
+            <ul className={styles.nav}>
+                <li className={`${activeTab === "Overview" ? styles.active : styles.inactive} ${styles.item}`} onClick={() => changeActiveTab("Overview")}>Overview</li>
+                <li className={`${activeTab === "General" ? styles.active : styles.inactive} ${styles.item}`} onClick={() => changeActiveTab("General")}>General</li>
+                <li className={`${activeTab === "Team A" ? styles.active : styles.inactive} ${styles.item}`} onClick={() => changeActiveTab("Team A")}>Team A</li>
+                <li className={`${activeTab === "Team B" ? styles.active : styles.inactive} ${styles.item}`} onClick={() => changeActiveTab("Team B")}>Team B</li>
+                <li className={`${activeTab === "Player" ? styles.active : styles.inactive} ${styles.item}`} onClick={() => changeActiveTab("Player")}>Player</li>
+            </ul>
+            <div className={activeTab !== "Overview" ? styles.hide : ''}>
+                <h4>Overview</h4>
+                <p>Work in progress Overview</p>
+            </div>
+            <div className={activeTab !== "General" ? styles.hide : ''}>
+                <h4>General</h4>
+                <p>Work in progress General</p>
+            </div>
+            <div className={activeTab !== "Team A" ? styles.hide : ''}>
+                <h4>Team A</h4>
+                <p>Work in progress Team A</p>
+            </div>
+            <div className={activeTab !== "Team B" ? styles.hide : ''}>
+                <h4>Team B</h4>
+                <p>Work in progress Team B</p>
+            </div>
+            <div className={activeTab !== "Player" ? styles.hide : ''}>
+                <h4>Player</h4>
+                <p>Work in progress Player</p>
+            </div>
+        </>
+    )
 }
