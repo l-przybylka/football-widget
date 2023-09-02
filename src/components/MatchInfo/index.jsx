fc6d6f6991b9d9ca3ba20444b95a6845a2e212bf
Lukasz <lukaszprzybylka@gmail.com>
2023-09-02 13:33:15 +0100

    style: intial styling of the match info component

--- src/components/MatchInfo/index.jsx
+++ src/components/MatchInfo/index.jsx
@@ -1,32 +1,37 @@
+import styles from "./MatchInfo.module.css"
 import Image from "next/image"
 
 export const MatchInfo = () => {
     return (
         <>
-            <div>
+            <div className={styles.header}>
                 <h2>Premier League</h2>
                 <h3>Full Time</h3>
             </div>
-            <div>
-                <p>team on the left</p>
-                <Image
-                    src="/assets/arsenalLogo.png"
-                    alt="arsenal logo"
-                    width={150}
-                    height={150}
-                />
-                <h4>4</h4>
+            <div className={styles.container}>
+                <div className={styles.team}>
+                    <Image
+                        src="/assets/arsenalLogo.png"
+                        alt="arsenal logo"
+                        width={150}
+                        height={150}
+                    />
+                    <p>Arsenal</p>
+                    <h4>Score: 4</h4>
+                </div>
+                <div className={styles.team}>
+
+                    <Image
+                        src="/assets/fulhamLogo.png"
+                        alt="fulham logo"
+                        width={150}
+                        height={150}
+                    />
+                    <p>Fulham</p>
+                    <h4>Score: 2</h4>
+                </div>
             </div>
-            <div>
-                <p>team on the right</p>
-                <Image
-                    src="/assets/fulhamLogo.png"
-                    alt="fulham logo"
-                    width={150}
-                    height={150}
-                />
-                <h4>2</h4>
-            </div>
+
         </>
     )
 }
