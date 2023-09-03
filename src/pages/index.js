ea7e46a168b86e9978ab27905d505f44ecefca2c
Lukasz <lukaszprzybylka@gmail.com>
2023-09-03 10:03:41 +0100

    feat/refactor: I've run into some issues with separating fetch from the index. This now sits in the index.js. Prepared state for all the required data

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,24 @@ import { Inter } from 'next/font/google'
 import { Container } from '@/components/Container'
 import { MatchResults } from '@/components/MatchResults'
 import { MatchDetails } from '@/components/MatchDetails'
+import { useEffect, useState } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
+  const [teamDetails, setTeamDetails] = []
+  const [competitionDetails, setCompetitionDetails] = []
+  const [scores, setScores] = []
+  const [statistics, setStatistics] = []
+
+    // How to separate this to a different file?
+    useEffect(() => {
+      fetch("https://frontend-tech-test-ashy.vercel.app/api/match")
+        .then(response => response.json())
+        .then(data => console.log(data))
+    }, [])
+
   return (
     <>
       <Head>
--- src/services/matchInfo.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-const url = "https://frontend-tech-test-ashy.vercel.app/api/match"
-
-const getAllData = async () => {
-    const request = await axios.get(url)
-    return request.then(res => res.data)
-}
