1cebd781c1c8b140d07e6a97fe75591d6763f496
Lukasz <lukaszprzybylka@gmail.com>
2023-09-03 09:06:25 +0100

    feat: added single statistic component with static info for initial styling

--- src/components/MatchStatistics/index.jsx
+++ src/components/MatchStatistics/index.jsx
@@ -1,4 +1,7 @@
+import { SingleStatistic } from "../SingleStatistic"
+
 export const MatchStatistics = () => {
+  // state will hold which part of the match we are in full time 1st or 2nd half
   return (
     <>
     <h5>General</h5>
@@ -6,7 +9,10 @@ export const MatchStatistics = () => {
     <button>1st half</button>
     <button>2nd half</button>
     <div>
-        <span>placeholder for stats</span>
+        <SingleStatistic statName={"Possesion"} homeTeamName={"Arsenal"} awayTeamName={"Fulham"} />
+        <SingleStatistic statName={"Shots on Target"} homeTeamName={"Arsenal"} awayTeamName={"Fulham"} />
+        <SingleStatistic statName={"Shots"} homeTeamName={"Arsenal"} awayTeamName={"Fulham"} />
+        <SingleStatistic statName={"Corners"} homeTeamName={"Arsenal"} awayTeamName={"Fulham"} />
     </div>
     </>
   )
---
+++ src/components/SingleStatistic/index.jsx
@@ -0,0 +1,16 @@
+export const SingleStatistic = ({ statName, homeTeamName, homeTeamStat, awayTeamName, awayTeamStat}) => {
+  return (
+    <>
+        <h5>{statName}</h5>
+        <div>
+            <span>{homeTeamName}</span>
+            <span>{homeTeamStat}</span>
+        </div>
+        <div>
+            <span>{awayTeamName}</span>
+            <span>{awayTeamStat}</span>
+        </div>
+
+    </>
+  )
+}
